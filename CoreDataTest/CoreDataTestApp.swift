7d88f9f0fd56c373c2354c63d6aabae6392c1331
Jacek Kosinski <kashiash@gmail.com>
2023-05-03 09:09:27 +0200

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by Jacek Kosiński G on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
